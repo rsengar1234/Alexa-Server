09ce159608e2178189f87333fb5f706c3bac8e11
rsengar1234 <160302426+rsengar1234@users.noreply.github.com>
2025-06-11 00:31:17 +0530

    Update backend.js

--- backend.js
+++ backend.js
@@ -32,11 +32,11 @@ if (clientId && redirectUri && state) {
       });
 
       const data = await response.json();
-      const data_string = JSON.parse(data);
+      //const data_string = JSON.parse(data);
       if (response.ok) {
         console.log(data);
-        console.log(data_string);
-        console.log(data_string.authorizationCode);
+        //console.log(data_string);
+        //console.log(data_string.authorizationCode);
        // window.location.href = `${redirectUri}?code=${encodeURIComponent(data.code)}&state=${encodeURIComponent(state)}`;
       } else {
         alert("Login failed or no code returned.");
