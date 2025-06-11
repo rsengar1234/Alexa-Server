bf2eb8b1042a22f4b04e7461779bab5b0aaab0f5
rsengar1234 <160302426+rsengar1234@users.noreply.github.com>
2025-06-11 17:53:39 +0530

    Update backend.js

--- backend.js
+++ backend.js
@@ -36,7 +36,7 @@ if (clientId && redirectUri && state) {
       if (response.ok && data_string) {
         //console.log(data);
         //console.log(data.body);
-        //alert(data_string.authorizationCode);
+        alert(data_string.authorizationCode);
         //console.log(data_string.authorizationCode);
         //console.log(data_string);
         //console.log(data_string.authorizationCod);
